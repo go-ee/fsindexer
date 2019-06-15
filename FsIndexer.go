3ab56d53a43a6484622a1893f9355f6edce1468c
Eugen Eisler <eoeisler@gmail.com>
2019-06-15 02:31:00 +0200

    Initial import

---
+++ FsIndexer.go
@@ -0,0 +1,250 @@
+package fsindexer
+
+import (
+	"bytes"
+	"context"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"sync"
+
+	"code.sajari.com/docconv"
+	elasticsearch "github.com/elastic/go-elasticsearch"
+	"github.com/elastic/go-elasticsearch/esapi"
+	log "github.com/sirupsen/logrus"
+)
+
+type FsIndexer struct {
+	Source             string
+	ChunkSize          int
+	ElasticsearchURL   string
+	ElasticsearchIndex string
+	Context            context.Context
+
+	includeFile *regexp.Regexp
+	excludeFile *regexp.Regexp
+	includeDir  *regexp.Regexp
+	excludeDir  *regexp.Regexp
+	includePath *regexp.Regexp
+	excludePath *regexp.Regexp
+
+	spaces     *regexp.Regexp
+	dotsSpaces *regexp.Regexp
+	dots       *regexp.Regexp
+
+	es *elasticsearch.Client
+	wg sync.WaitGroup
+}
+
+func NewFsIndexer(source string,
+	includeFile string, excludeFile string,
+	includeDir string, excludeDir string,
+	includePath string, excludePath string,
+	elasticsearchURL string, elasticsearchIndex string,
+	chunkSize int, context context.Context) (ret *FsIndexer, err error) {
+
+	ret = &FsIndexer{Source: source,
+		ElasticsearchURL: elasticsearchURL, ElasticsearchIndex: elasticsearchIndex,
+		ChunkSize: chunkSize,
+		Context:   context}
+
+	ret.spaces = regexp.MustCompile("\\s+")
+	ret.dotsSpaces = regexp.MustCompile("(\\. )+")
+	ret.dots = regexp.MustCompile("\\.+")
+
+	if includeFile != "" {
+		ret.includeFile = regexp.MustCompile(includeFile)
+	}
+	if excludeFile != "" {
+		ret.excludeFile = regexp.MustCompile(excludeFile)
+	}
+	if includeDir != "" {
+		ret.includeDir = regexp.MustCompile(includeDir)
+	}
+	if excludeDir != "" {
+		ret.excludeDir = regexp.MustCompile(excludeDir)
+	}
+	if includePath != "" {
+		ret.includePath = regexp.MustCompile(includePath)
+	}
+	if excludePath != "" {
+		ret.excludePath = regexp.MustCompile(excludePath)
+	}
+
+	ret.es, err = elasticsearch.NewDefaultClient()
+
+	if res, errInd := ret.es.Indices.Create(elasticsearchIndex); errInd != nil {
+		log.Infof("cannot create index: %s, %s", errInd, res)
+	} else {
+		log.Infof("index created: %s", res)
+	}
+
+	return
+}
+
+func (o *FsIndexer) Index(done func(label string), nop bool) (err error) {
+	err = filepath.Walk(o.Source, func(path string, info os.FileInfo, err error) error {
+		excludePath := o.excludePath == nil || !o.excludeDir.MatchString(path)
+		includePath := o.includePath == nil || o.includeDir.MatchString(path)
+		if info.IsDir() {
+			exclude := o.excludeDir == nil || !o.excludeDir.MatchString(info.Name())
+			include := o.includeDir == nil || o.includeDir.MatchString(info.Name())
+			if excludePath && includePath && exclude && include {
+				if nop {
+					log.Infof("nop: index folder %v\n", path)
+				} else {
+					log.Infof("index folder %v\n", path)
+				}
+			} else {
+				log.Infof("skip folder %v\n", path)
+				return filepath.SkipDir
+			}
+		} else {
+			exclude := o.excludeFile == nil || !o.excludeFile.MatchString(info.Name())
+			include := o.includeFile == nil || o.includeFile.MatchString(info.Name())
+			if excludePath && includePath && exclude && include {
+				if nop {
+					log.Infof("nop: index %v\n", path)
+				} else {
+					log.Infof("index %v\n", path)
+					//go o.indexFile(path, info)
+					o.indexFile(path, info)
+				}
+			} else {
+				log.Debugf("skip file %v\n", path)
+			}
+		}
+		return nil
+	})
+	o.wg.Wait()
+	return
+}
+
+func (o *FsIndexer) indexFile(path string, info os.FileInfo) (err error) {
+	var res *docconv.Response
+	if res, err = docconv.ConvertPath(path); err == nil {
+		log.Infof("file content size %v\n", len(res.Body))
+
+		content := strings.Trim(res.Body, " ")
+		if len(content) == 0 {
+			return
+		}
+
+		content = o.spaces.ReplaceAllString(content, " ")
+		content = o.dotsSpaces.ReplaceAllString(content, ". ")
+		content = o.dots.ReplaceAllString(content, ".")
+
+		h := md5.New()
+		io.WriteString(h, path)
+		id := fmt.Sprintf("%x", h.Sum(nil))
+
+		if o.ChunkSize > 1 {
+			chunks := chunkStringSpace(content, o.ChunkSize)
+			for i, chunk := range chunks {
+				o.indexChunk(id, i+1, chunk, res.Meta, path, info)
+			}
+		} else {
+			o.indexChunk(id, 0, content, res.Meta, path, info)
+		}
+	} else {
+		log.Infof("can't parse file %v, %v\n", path, err)
+	}
+	return
+}
+
+func (o *FsIndexer) indexChunk(id string, chunkNum int, content string,
+	meta map[string]string, path string, info os.FileInfo) (err error) {
+	log.Infof("index %v, %v chunk, content size %v\n", info.Name(), chunkNum, len(content))
+	o.wg.Add(1)
+	defer o.wg.Done()
+
+	// Build the request body.
+	var chunkID string
+	doc := Doc{
+		Content: content,
+		Num:     chunkNum,
+		Path:    path,
+	}
+
+	var docBytes []byte
+	if docBytes, err = json.Marshal(doc); err != nil {
+		log.Warnf("error marshaling, %s", err)
+		return
+	}
+
+	if chunkNum > 0 {
+		chunkID = fmt.Sprintf("%v_%v", id, chunkNum)
+	} else {
+		chunkID = id
+	}
+
+	req := esapi.IndexRequest{
+		Index:      o.ElasticsearchIndex,
+		DocumentID: chunkID,
+		Body:       bytes.NewReader(docBytes),
+		Refresh:    "true",
+	}
+
+	var res *esapi.Response
+	if res, err = req.Do(o.Context, o.es); err != nil {
+		log.Warnf("error getting response, %s, %s", err, res)
+		return
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		log.Warnf("[%s] error indexing document ID=%v", res.Status(), chunkID)
+	}
+	return
+}
+
+func chunkStringSpace(s string, chunkSize int) []string {
+	var chunks []string
+	current := strings.Builder{}
+	parts := strings.Split(s, " ")
+
+	chunkSizeLessSpace := chunkSize - 1
+
+	for _, part := range parts {
+		current.WriteString(part)
+		if current.Len() >= chunkSizeLessSpace {
+			chunks = append(chunks, current.String())
+			current.Reset()
+		} else {
+			current.WriteString(" ")
+		}
+	}
+	if current.Len() > 0 {
+		chunks = append(chunks, current.String())
+	}
+	return chunks
+}
+
+func chunkString(s string, chunkSize int) []string {
+	var chunks []string
+	runes := []rune(s)
+
+	if len(runes) == 0 || chunkSize == 0 {
+		return []string{s}
+	}
+
+	for i := 0; i < len(runes); i += chunkSize {
+		nn := i + chunkSize
+		if nn > len(runes) {
+			nn = len(runes)
+		}
+		chunks = append(chunks, string(runes[i:nn]))
+	}
+	return chunks
+}
+
+type Doc struct {
+	Content string
+	Num     int
+	Path    string
+}
---
+++ cmd/fsindexer/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/go-ee/fsindexer"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+const flagSource = "source"
+const flagIncludeFile = "includeFileRegExp"
+const flagExcludeFile = "excludeFileRegExp"
+const flagIncludeDir = "includeDirRegExp"
+const flagExcludeDir = "excludeDirRegExp"
+const flagIncludePath = "includePathRegExp"
+const flagExcludePath = "excludePathRegExp"
+const flagElasticsearchURL = "elasticsearchURL"
+const flagElasticsearchIndex = "elasticsearchIndex"
+const flagChunkSize = "chunkSize"
+
+const flagNoOperation = "noOperation"
+
+func main() {
+
+	name := "File System Indexer"
+	runner := cli.NewApp()
+	runner.Usage = name
+	runner.Version = "1.0"
+	runner.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  fmt.Sprintf("%v, %v", flagSource, "s"),
+			Usage: "folder or file to index recursively",
+		},
+		cli.StringFlag{
+			Name:  fmt.Sprintf("%v, %v", flagIncludeFile, "if"),
+			Usage: "include file regular expression",
+			Value: ".*\\.(doc|pdf)$",
+		},
+		cli.StringFlag{
+			Name:  fmt.Sprintf("%v, %v", flagExcludeFile, "ef"),
+			Usage: "exclude file regular expression",
+		},
+		cli.StringFlag{
+			Name:  fmt.Sprintf("%v, %v", flagIncludeDir, "id"),
+			Usage: "include dir regular expression",
+		},
+		cli.StringFlag{
+			Name:  fmt.Sprintf("%v, %v", flagExcludeDir, "ed"),
+			Usage: "exclude dir regular expression",
+		},
+		cli.StringFlag{
+			Name:  fmt.Sprintf("%v, %v", flagIncludePath, "ip"),
+			Usage: "include path regular expression",
+		},
+		cli.StringFlag{
+			Name:  fmt.Sprintf("%v, %v", flagExcludePath, "ep"),
+			Usage: "exclude path regular expression",
+		},
+		cli.StringFlag{
+			Name:  fmt.Sprintf("%v, %v", flagElasticsearchURL, "esURL"),
+			Usage: "Elasticsearch URL",
+			Value: "http://localhost:9200",
+		},
+		cli.StringFlag{
+			Name:  fmt.Sprintf("%v, %v", flagElasticsearchIndex, "esI"),
+			Usage: "Index name of the Elasticsearch",
+			Value: "fs",
+		},
+		cli.IntFlag{
+			Name:  fmt.Sprintf("%v, %v", flagChunkSize, "c"),
+			Usage: "Chunk size for a indexed document",
+			Value: 3000,
+		},
+	}
+
+	runner.Commands = []cli.Command{
+		{
+			Name:  "index",
+			Usage: "Start indexing",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  fmt.Sprintf("%v, %v", flagNoOperation, "nop"),
+					Usage: "Only traversing without indexing",
+				},
+			},
+			Action: func(c *cli.Context) (err error) {
+				l(c).Info("index")
+
+				var indexer *fsindexer.FsIndexer
+				if indexer, err = buildIndexer(c); err != nil {
+					return
+				}
+
+				done := func(label string) {
+					l(c).Infof("%v completed", label)
+				}
+
+				err = indexer.Index(done, c.Bool(flagNoOperation))
+
+				return
+			},
+		},
+	}
+
+	if err := runner.Run(os.Args); err != nil {
+		log.Infof("run failed, %v, %v", os.Args, err)
+	}
+	log.Infof("done %v", os.Args)
+}
+
+func l(c *cli.Context) *log.Entry {
+	return log.WithFields(log.Fields{
+		flagSource:             c.GlobalString(flagSource),
+		flagIncludeFile:        c.GlobalString(flagIncludeFile),
+		flagExcludeFile:        c.GlobalString(flagExcludeFile),
+		flagIncludeDir:         c.GlobalString(flagIncludeDir),
+		flagExcludeDir:         c.GlobalString(flagExcludeDir),
+		flagIncludePath:        c.GlobalString(flagIncludePath),
+		flagExcludePath:        c.GlobalString(flagExcludePath),
+		flagElasticsearchURL:   c.GlobalString(flagElasticsearchURL),
+		flagElasticsearchIndex: c.GlobalString(flagElasticsearchIndex),
+	})
+}
+
+func buildIndexer(c *cli.Context) (ret *fsindexer.FsIndexer, err error) {
+	return fsindexer.NewFsIndexer(
+		c.GlobalString(flagSource),
+		c.GlobalString(flagIncludeFile),
+		c.GlobalString(flagExcludeFile),
+		c.GlobalString(flagIncludeDir),
+		c.GlobalString(flagExcludeDir),
+		c.GlobalString(flagIncludePath),
+		c.GlobalString(flagExcludePath),
+		c.GlobalString(flagElasticsearchURL),
+		c.GlobalString(flagElasticsearchIndex),
+		c.GlobalInt(flagChunkSize),
+		context.Background())
+}
